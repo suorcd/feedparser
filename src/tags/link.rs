1eb9f7200c52164f6304176f58f88546e7005a47
Eric P <ericpp@gmail.com>
2025-12-14 18:04:56 -0600

    Set description, generator, and link if not already set

--- src/tags/link.rs
+++ src/tags/link.rs
@@ -2,8 +2,12 @@ use crate::parser_state::ParserState;
 
 pub fn on_text(data: &str, state: &mut ParserState) {
     if state.in_item {
-        state.link.push_str(data);
+        if state.link.is_empty() {
+            state.link.push_str(data);
+        }
     } else if state.in_channel && !state.in_channel_image {
-        state.channel_link.push_str(data);
+        if state.channel_link.is_empty() {
+            state.channel_link.push_str(data);
+        }
     }
 }
